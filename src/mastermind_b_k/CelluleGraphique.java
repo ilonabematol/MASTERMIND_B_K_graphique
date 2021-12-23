331e71b06023408c10c376965f5a6cbc1bda7f52
ilonabematol <ilonabematol@192.168.1.136>
2021-12-23 12:50:48 +0100

    début du projet en graphique

---
+++ src/mastermind_b_k/CelluleGraphique.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_b_k;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author ilonabematol
+ */
+public class CelluleGraphique extends JButton {
+    Grille_principale Pions;
+    
+}
